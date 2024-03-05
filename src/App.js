823e4222f6f5c4c5bb4466254abd4f76a0f4450a
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-03-06 00:53:41 +0200

    asdad

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './assets/logo.png';
 import './App.css';
 import TG from "./assets/TG.png"
 import TW from "./assets/TW.png"
-import batjok from "./assets/batjok.png"
 
 function App() {
   return (
@@ -10,8 +9,7 @@ function App() {
       <div className="section1">
         <div className="main">
           <img alt="logo" className="logo" src={logo}/>
-          <h1 className="heading">Bootman VS Jokar</h1>
-          <div className="batjockBlock"><img alt="batjok" className="batjok" src={batjok}/></div>
+          <h1 className="heading">COMMING SOON</h1>
           <div className="socialsBlock">
             <a href="https://t.me/bootmanvsjokar" target="_blank" rel="noopener noreferrer"><img className="social" alt="TG" src={TG} /></a>
             <a href="https://twitter.com/bootmanvsjokar" target="_blank" rel="noopener noreferrer"><img className="social" alt="TW" src={TW} /></a>
