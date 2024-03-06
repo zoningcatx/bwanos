19a2bf82d9bae39a5bf5d3220595a5baf7f87ba2
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-03-06 03:21:31 +0200

    asdasd

--- src/App.js
+++ src/App.js
@@ -9,11 +9,29 @@ function App() {
       <div className="section1">
         <div className="main">
           <img alt="logo" className="logo" src={logo}/>
-          <h1 className="heading">COMMING SOON</h1>
+          <h1 className="heading">PLOMO O PLATA</h1>
           <div className="socialsBlock">
             <a href="https://t.me/bwanos" target="_blank" rel="noopener noreferrer"><img className="social" alt="TG" src={TG} /></a>
             <a href="https://twitter.com/bwanos_" target="_blank" rel="noopener noreferrer"><img className="social" alt="TW" src={TW} /></a>
           </div>
+          <button className="button">BUY $BWANOS</button>
+        </div>
+      </div>
+      <div className="section2">
+        <h2 className="heading2">THE ORDEER</h2>
+        <div className="desc">
+          <h2 className="heading3">SUPPLY: 900M KG</h2>
+          <h2 className="heading3">DELIVERY: 6TH MARCH</h2>
+        </div>
+      </div>
+      <div className="section3">
+        <div className="divider">
+        </div>
+      </div>
+      <div className="section4">
+        <div className="footer">
+          <a href="https://t.me/bwanos" target="_blank" rel="noopener noreferrer"><img className="social" alt="TG" src={TG} /></a>
+          <a href="https://twitter.com/bwanos_" target="_blank" rel="noopener noreferrer"><img className="social" alt="TW" src={TW} /></a>
         </div>
       </div>
     </div>
